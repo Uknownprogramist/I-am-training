64f2316cb902f2b2d32b85fe47ad094974939fb0
Uknownprogramist <73348358+Uknownprogramist@users.noreply.github.com>
2020-10-23 19:53:42 +0500

    Add files via upload

---
+++ Source.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+using namespace std;
+int main() {
+	int first, second, summary;
+	cin >> first;
+	cout << "\n";
+	cin >> second;
+	summary = first + second;
+	cout << "\n" << summary;
+}
